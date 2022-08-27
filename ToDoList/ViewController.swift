68bb03f3e0a555a7df53de133e7849c1cf54c532
Nikita Lukyantsev <lnik18@mail.ru>
2022-08-27 13:02:01 +0300

    Иконка и версия

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Nikita Lukyantsev on 27.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var buildLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            buildLabel.text = "Version \(appVersion)"
+        }
+    }
+    
+    
+}
+
